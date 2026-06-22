f51243378330a43efa9255d7c0da6c35e10e1cf9
Artualist <102818608+artualist@users.noreply.github.com>
2026-06-22 15:23:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ PAYMENT_WALLET: str = os.environ.get("PAYMENT_WALLET", "")
 PREMIUM_PRICE_CENTS: int = int(os.environ.get("PREMIUM_PRICE_CENTS", "1999"))
 FREE_TIER_LIMIT: int = int(os.environ.get("FREE_TIER_LIMIT", "3"))
 POLL_INTERVAL: int = int(os.environ.get("POLL_INTERVAL", "30"))
-DEFAULT_USD_THRESHOLD: float = float(os.environ.get("DEFAULT_USD_THRESHOLD", "1000000"))
+DEFAULT_USD_THRESHOLD: float = float(os.environ.get("DEFAULT_USD_THRESHOLD", "100000"))
 PAYMENT_TTL_SECONDS: int = int(os.environ.get("PAYMENT_TTL_SECONDS", "1800"))
 
 LIFETIME_PREMIUM_USERS: frozenset[str] = frozenset({"artualist", "artualista"})
